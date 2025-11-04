24939ee286112675d09548d3267b694b6efb69a5
vader113 <saqibjarvis11+1@gmail.com>
2025-11-04 23:38:25 +0500

    added images

--- Android.mk
+++ Android.mk
@@ -6,5 +6,17 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),NOTE_23)
 
+$(call add-radio-file-sha1-checked,radio/dpm.img,c59ca3fc9b2bd78e70b7f385a38bb10e1921bcad)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,9db156ad3489f7131ead68d955b289c3f8a23a97)
+$(call add-radio-file-sha1-checked,radio/gz.img,821919f532689f68949dde666ffd8ea49b9b6f3f)
+$(call add-radio-file-sha1-checked,radio/lk.img,13043a535bed4e8b18507443ed18e71d2ec52087)
+$(call add-radio-file-sha1-checked,radio/logo.img,3f3b7d91bbcad905866ca090e79d55b262a0b4fc)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,60fdfa336a8ad08e847f5721fdd13ab69aa6fb87)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,91d66211e4031c81dcf231c072e3907ce13a7a9c)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,1686454288786c3be42334fe905504b68d66a2f6)
+$(call add-radio-file-sha1-checked,radio/scp.img,f2fe989f25d030993d847f3d812de03c60bb58be)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,974ea19f5a9839a5aa87ccb1f380354c54cb87bd)
+$(call add-radio-file-sha1-checked,radio/sspm.img,5d6da887dc66effc12b3af3013307580f76a4d61)
+$(call add-radio-file-sha1-checked,radio/tee.img,21a204c9f2e0d8a8b71d0fe18106d2bedb9e6691)
 
 endif
